6c260df4c6b56100f681029ccca660811a1b3d9e
gaoxiankang <944168348@qq.com>
2020-03-27 18:33:11 +0800

    backtrack

--- backtrack/combinations.go
+++ backtrack/combinations.go
@@ -17,21 +17,34 @@ Output:
 ]
 */
 func Combine(n int, k int) [][]int {
-
+	combine(n, k)
 }
 
 // 列出C(n,k)的所有值
 func combine(n int, k int) [][]int {
+	if k > n {
+		return nil
+	}
 	src := make([]int, n)
 	for i := 0; i < n; i++ {
 		src[i] = i + 1
 	}
-
+	return next(nil, src, nil, k)
 }
 
-func next(result [][]int, src []int, extra int) []int {
+func next(result [][]int, src []int, left []int, extra int) [][]int {
 	if extra == 0 {
 		result = append(result, src)
 		return result
 	}
+	var s []int
+	for i, v := range src {
+		// 向s中添加最后一个元素
+		s = append(s, v)
+		left = src[i+1:]
+		result = next(result, s, left, extra-1)
+		// 移除最有一个元素
+		s = s[:i]
+	}
+	return result
 }
